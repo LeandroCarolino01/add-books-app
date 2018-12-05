60787ec9da6aa25b6e24ab79050254d68f20b020
leandro <leandro@isset.nl>
2018-12-05 18:08:05 +0100

    data added to the dom

--- js/app.js
+++ js/app.js
@@ -10,7 +10,25 @@ function UI() {}
 
 // Add Book To Function
 UI.prototype.addBookToList = function(book){
-    console.log(book);
+    const list = document.getElementById('book-list');
+
+    const row = document.createElement('tr')
+
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X</a></td>
+`
+    list.appendChild(row);
+}
+// clear fields
+UI.prototype.clearFields = function(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+    
+
 }
 
 
@@ -26,12 +44,14 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     const book = new Book(title, author, isbn);
 
     //Instantiate book
-    const ui = new Book(title, author, isbn);
+    const ui = new UI();
 
-    console.log(ui);
+
 
     // Add book to list
     ui.addBookToList(book);
+
+    ui.clearFields();
 
     e.preventDefault();
 })
